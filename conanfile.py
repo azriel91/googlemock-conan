804d0767201ad9f831fb1aade3ebd2effe7329d8
Azriel Hoh <azriel91@gmail.com>
2016-02-15 18:34:42 +1300

    Added license to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ import os
 
 class GoogleMockConan(ConanFile):
     name = 'googlemock'
+    license = 'Copyright 2008, Google Inc.'  # See https://github.com/google/googlemock/blob/release-1.7.0/LICENSE
     version = '1.7.0'
     settings = ['os', 'compiler', 'build_type', 'arch']
     generators = ['cmake']
